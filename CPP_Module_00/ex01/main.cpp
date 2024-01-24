a2a16b6a249197be408fb77950bea844b3981e61
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-25 05:34:19 +0700

    update main.cpp

--- CPP_Module_00/ex01/main.cpp
+++ CPP_Module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 02:19:41 by ookamonu          #+#    #+#             */
-/*   Updated: 2024/01/25 05:18:59 by ookamonu         ###   ########.fr       */
+/*   Updated: 2024/01/25 05:33:41 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(void)
 	std::string input;
 	int lastAddedIndex = 0;
 
-	std::cout << "Input: ";
+	std::cout << "Input one operation (ADD / SEARCH / EXIT): ";
 	while (std::getline(std::cin, input))
 	{
 		if (input == "ADD")
@@ -37,9 +37,9 @@ int main(void)
 		}
 		else
 		{
-			std::cout << "\n\tWrong input!\n\tEnter: ADD or SEARCH or EXIT\n\tWith no spaces\n" << std::endl;
+			std::cout << "\n\tInvalid input!!\n" << std::endl;
 		}
-		std::cout << "Input: ";
+		std::cout << "Input one operation (ADD / SEARCH / EXIT): ";
 	}
 	return (0);
 }
