a262350abea9e064d914df327c7e03ef86431c42
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-25 05:35:37 +0700

    update PhoneBook.hpp

--- CPP_Module_00/ex01/PhoneBook.hpp
+++ CPP_Module_00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 02:19:39 by ookamonu          #+#    #+#             */
-/*   Updated: 2024/01/25 04:03:52 by ookamonu         ###   ########.fr       */
+/*   Updated: 2024/01/25 05:35:31 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ class PhoneBook
 	public:
 		PhoneBook();
 		~PhoneBook();
-		void	Search() const;
+		void Search() const;
 
-		Contact cont[8];
+		Contact	cont[8];
 };
 
 #endif
