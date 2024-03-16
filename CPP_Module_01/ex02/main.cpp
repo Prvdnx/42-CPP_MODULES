843db767dbba790c02bd78794ab0cd99d74c1039
Oghenefega Okamonu <ookamonu@z1t1c2.42bangkok.com>
2024-03-17 03:55:44 +0700

    update

--- CPP_Module_01/ex02/main.cpp
+++ CPP_Module_01/ex02/main.cpp
@@ -23,9 +23,9 @@ This is all about the similarities & differences between pointers & references.
 
 int	main()
 {
-	std::string str = "HI THIS IS BRAIN";
-	std::string *stringPTR = &str;
-	std::string &stringREF = str;
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
 
 	std::cout << std::endl;
 	std::cout << "The memory address of the string variable str: " << &str << std::endl;
@@ -35,7 +35,7 @@ int	main()
 	std::cout << std::endl;
 	std::cout << "The value of the string variable str: " << str << std::endl;
 	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
-	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << "\n" << std::endl;
 
 	return (0);
 }
