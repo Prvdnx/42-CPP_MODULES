5155bf2e84c92dfda192c6d5d66166ee68196c6a
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-03-19 05:08:52 +0700

    all

--- CPP_Module_02/ex00/Fixed.hpp
+++ CPP_Module_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 00:02:46 by ookamonu          #+#    #+#             */
-/*   Updated: 2024/03/19 04:39:32 by ookamonu         ###   ########.fr       */
+/*   Updated: 2024/03/19 05:07:30 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,18 @@
 
 # include <iostream>
 
-/*class Fixed
+class Fixed
 {
-    private:
-        int _value;
-        static const int _fractionalBits = 8;
-
-    public:
-        Fixed(); //default constructor
-        Fixed(const Fixed &other); //copy constructor
-        Fixed &operator=(const Fixed &other); //copy assignment operator
-        ~Fixed(); // Destructor
-
-        int     getRawBits() const;
-        void    setRawBits(int const raw);
-};
-
-#endif*/
-
-class Fixed {
-public:
-    Fixed();
-    Fixed(const Fixed &other);
-    Fixed &operator=(const Fixed &other);
-    ~Fixed();
-    int getRawBits() const;
-    void setRawBits(int const raw);
-
-private:
-    int _value;
-    static const int _frac_bits = 8;
+	private:
+		int _value;
+		static const int _frac_bits = 8;    
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed  	&operator=(const Fixed &other);
+		~Fixed();
+		int getRawBits() const;
+		void    setRawBits(int const raw);
 };
 
 #endif
