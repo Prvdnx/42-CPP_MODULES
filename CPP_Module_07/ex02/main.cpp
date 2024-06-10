79998453a39130516ad8845b9c01d417c0925e93
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-06-10 22:20:42 +0700

    update 1 file and delete 1 file

--- CPP_Module_07/ex02/main.cpp
+++ CPP_Module_07/ex02/main.cpp
@@ -1,53 +1,43 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 18:22:39 by ookamonu          #+#    #+#             */
+/*   Updated: 2024/06/10 22:14:52 by ookamonu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int	main()
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	try
+	{
+		Array<int> arr(12);	// create array of int obj of 5 elements
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+		std::cout << "Initial values: ";	// print initial/garbage values
+		for (unsigned int i = 0; i < arr.size(); i++)
+			std::cout << arr[i] << " ";
+		std::cout << std::endl;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+		for (unsigned int i = 0; i < arr.size(); i++)	// modify values
+			arr[i] = i * 2;
+
+		std::cout << "Modified values: "; 	// print modified values
+		for (unsigned int i = 0; i < arr.size(); i++)
+			std::cout << arr[i] << " ";
+		std::cout << std::endl;
+
+		std::cout << "Accessing out of bounds element: ";	// access elements that are out of bound
+		std::cout << arr[16] << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	return (0);
 }
